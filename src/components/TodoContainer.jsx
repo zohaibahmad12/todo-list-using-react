1c573f517b0be8fb1a43556af6292310a9172f6c
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2023-08-05 00:06:22 +0500

    LocalStorage functionality implemented

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import TodoItem from "./TodoItem";
 import TodoButton from "./TodoButton";
 
+
+
+
+
+
 const TodoContainer = () => {
 
+    const getLocalStorageTodoList = () => {
+
+        const todoList = JSON.parse(localStorage.getItem("todoList"));
+        console.log("Function Run");
+        return (todoList) ? todoList : [];
+
+    }
+
     const [newItemText, setNewItemText] = useState("");
-    const [allItems, setAllItems] = useState([]);
+    const [allItems, setAllItems] = useState(getLocalStorageTodoList());
+
+
+    useEffect(() => {
+
+        localStorage.setItem("todoList", JSON.stringify(allItems));
+
+    }, [allItems])
+    
 
     const handleInputChange = (event) => {
 
@@ -24,14 +45,15 @@ const TodoContainer = () => {
             const newArray = [...allItems, newItemText];
             setAllItems(newArray);
             setNewItemText("");
+
         }
 
     }
 
-    const deleteItem=(index)=>{
+    const deleteItem = (index) => {
 
         const newArray = [...allItems];
-        newArray.splice(index,1);
+        newArray.splice(index, 1);
         setAllItems(newArray);
     }
 
@@ -41,7 +63,7 @@ const TodoContainer = () => {
 
             <TodoButton newItemText={newItemText} onChange={handleInputChange} onSubmit={addNewItemToList} />
 
-            {allItems.map((currentValue,currentIndex) => <TodoItem key={currentIndex} itemText={currentValue} itemId={currentIndex} deleteItem={deleteItem} />)}
+            {allItems.map((currentValue, currentIndex) => <TodoItem key={currentIndex} itemText={currentValue} itemId={currentIndex} deleteItem={deleteItem} />)}
 
         </div>
     )
